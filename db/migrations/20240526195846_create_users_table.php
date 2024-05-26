3ed2025768435de5f97b76c3e510dbec28cd1aaf
Ihor Rud <Igorryd7@gmail.com>
2024-05-26 20:34:49 +0000

    feat: create users table & remove migrations.sql & phpstan.neon.dist

---
+++ db/migrations/20240526195846_create_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateUsersTable extends AbstractMigration
+{
+    public function up(): void
+    {
+        $this->table('users')
+            ->addColumn('first_name', 'string', ['limit' => 255])
+            ->addColumn('last_name', 'string', ['limit' => 255, 'default' => null])
+            ->addColumn('user_name', 'string', ['limit' => 255, 'default' => null])
+            ->addColumn('is_premium', 'boolean', ['signed' => false])
+            ->addColumn('language_code', 'string', ['limit' => 10])
+            ->addColumn('chat_id', 'biginteger')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->save();
+    }
+
+    public function down(): void
+    {
+        $this->table('users')->drop()->save();
+    }
+}
