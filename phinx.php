3f42e29b661caa3486f09b3e908331d74f87c00b
Ihor Rud <Igorryd7@gmail.com>
2024-05-26 20:33:07 +0000

    feat: install symfony/validation & robmorgan/phinx

---
+++ phinx.php
@@ -0,0 +1,28 @@
+<?php
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'environment',
+        'environment' => [
+            'adapter' => 'mysql',
+            'host' => $_ENV['DB_HOST'],
+            'name' => $_ENV['DB_DATABASE'],
+            'user' => $_ENV['DB_USERNAME'],
+            'pass' => $_ENV['DB_PASSWORD'],
+            'port' => $_ENV['DB_PORT'],
+            'charset' => 'utf8',
+        ],
+    ],
+    'version_order' => 'creation'
+];
